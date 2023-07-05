162c73c51516977baa78d6d2a071b63594d79b97
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2023-07-05 18:50:50 +0500

    completed

--- setup.sh
+++ setup.sh
@@ -4,4 +4,13 @@ sudo apt update
 sudo apt upgrade
 sudo apt install python3-pip
 sudo apt install git
+sudo rm -r pid-led-ldr-with-RPI
+git clone https://github.com/manhoosbilli1/pid-led-ldr-with-RPI.git
+sudo rm -r project
+cp -r pid-led-ldr-with-RPI project
+cd project
+pip install -r req.txt
+sudo rm -r pid-led-ldr-with-RPI
+echo -e "\n\n\n\n\n You are all done. now just type switch to 'project' directory by cd project and type 'python script.py' and you will see output" 
+echo "Enjoy! good bye. "
 
